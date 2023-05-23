2e97fe3123ed0a4302dc26d33a9c32744b619c57
ManInDark <61268856+ManInDark@users.noreply.github.com>
2023-05-23 23:39:33 +0200

    Added fighting

--- constants.py
+++ constants.py
@@ -4,3 +4,29 @@ UP     = 450
 LEFT   = 452
 RIGHT  = 454
 DOWN   = 456
+
+DUNGEON_MONSTERS = [
+    [
+        {"name": "Slime", "health": 15, "health-max": 15, "attack": 1},
+        {"name": "Goblin", "health": 10, "health-max": 10, "attack": 4}
+    ]
+]
+DUNGEON_MONSTERS_LOOT = [
+    [
+        {"type": "sword", "level": 1},
+        {"type": "chestplate", "level": 1},
+        {"type": "coins", "amount": 2},
+        {"type": "coins", "amount": 3},
+        {"type": "coins", "amount": 4}
+    ]
+]
+DUNGEON_BOSSES = [
+    {"name": "Boss Goblin", "health": 30, "health-max": 30, "attack": 8}
+]
+DUNGEON_BOSSES_LOOT = [
+    [
+        {"type": "sword", "level": 1},
+        {"type": "chestplate", "level": 1},
+        {"type": "coins", "amount": 6}
+    ]
+]
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from constants import *
 from unicurses import *
+from random import choice
 stdscr = initscr()
 
 if __name__ != "__main__":
@@ -9,7 +10,7 @@ clear()
 curs_set(False)
 keypad(scr_id=stdscr, yes=True)
 
-PLAYER_DATA = {
+PLAYER_DATA = { # attack without anything is 1
     "coins": 0,
     "health": 100,
     "health-max": 100,
@@ -17,13 +18,22 @@ PLAYER_DATA = {
     "equipped": []
 }
 GAMESTATE = [0]
-TMP_DATA = [0] # [0] is used for selections
+TMP_DATA = [0] # is used for selections
 SELECTION_OPTIONS = [-1, 0] # [optioncount || -1 to deactivate, TMP_DATA index to store state]
+MESSAGES = []
+
+def calculateAttack() -> int:
+    return 1
+
+def calculateDefense() -> int:
+    return 1
 
 def print_header():
-    addstr(f"--- Coins: {PLAYER_DATA['coins']}\n")
-    addstr(f"--- Health: {PLAYER_DATA['health']}/{PLAYER_DATA['health-max']}\n")
-    addstr(f"---\n\n")
+    width = 9
+    addstr(f"---  Coins: {str(PLAYER_DATA['coins']).rjust(width)}   Attack: {str(calculateAttack()).rjust(width)}\n")
+    health = f"{PLAYER_DATA['health']}/{PLAYER_DATA['health-max']}".rjust(width)
+    addstr(f"--- Health: {health}  Defense: {str(calculateDefense()).rjust(width)}\n\n")
+    # TODO Attack and Defense
 
 def print_option_list (options):
     global SELECTION_OPTIONS
@@ -41,24 +51,39 @@ def print_gamestate():
 
     # DUNGEON
     if GAMESTATE[0] == 1:
+        # has to be if because GAMESTATE is gradually being filled, therefore they might be executed in series
         if len(GAMESTATE) == 1: # dungeon has not been chosen yet
             addstr("You stand in front of many doors, leading to many different dungeons. Which would you like to enter?\n\n")
             options = ["Dungeon 1", "Dungeon 2", "Dungeon 3", "Return"]
             print_option_list(options)
-        else: # fighting
-            pass
+        if len(GAMESTATE) == 2: # dungeon has just been entered; GAMESTATE = [1, DUNGEON_LEVEL]
+            GAMESTATE.append((GAMESTATE[1] + 1) * 5) # add how many enemies have to be fought to clear the dungeon
+        if len(GAMESTATE) == 3: # either upon dungeon creation or after defeating an enemy; GAMESTATE = [1, DUNGEON_LEVEL, ENEMIES_UNTIL_BOSS]
+            if GAMESTATE[2] > 0:
+                GAMESTATE.append(choice(DUNGEON_MONSTERS[GAMESTATE[1]]).copy())
+            elif GAMESTATE[2] == 0:
+                GAMESTATE.append(DUNGEON_BOSSES[GAMESTATE[1]].copy())
+        if len(GAMESTATE) == 4: # now we're fighting; GAMESTATE = [1, DUNGEON_LEVEL, ENEMIES_UNTIL BOSS, {}]; {} = current enemy
+            addstr(f"You are in the dungeon {GAMESTATE[1] + 1}. Having encountered the enemy number {(GAMESTATE[1] + 1) * 5 - GAMESTATE[2] + 1}, which is a {GAMESTATE[3]['name']}, what do you want to do?\n\n")
+            options = ["Attack", "Flee"]
+            print_option_list(options)
 
     # SMITHY
     if GAMESTATE[0] == 2:
         addstr("You are currently in the smithy. What would you like to do?\n\n")
-        pass
+    
+    addstr("\n")
+    for message in MESSAGES:
+        addstr(f"\n{message}")
 
 def handle_input(ch: int):
-    global GAMESTATE, SELECTION_OPTIONS
+    global GAMESTATE, SELECTION_OPTIONS, MESSAGES
     if ch == ESC: # quick exit
         print("Exited gracefully")
         exit()
-    elif SELECTION_OPTIONS[0] >= 0: # if in selection, just handle selection code
+    if ch == RETURN:
+        MESSAGES = []
+    if SELECTION_OPTIONS[0] >= 0: # if in selection, just handle selection code
         if ch == UP:
             TMP_DATA[SELECTION_OPTIONS[1]] = (TMP_DATA[SELECTION_OPTIONS[1]] + SELECTION_OPTIONS[0] - 1) % SELECTION_OPTIONS[0]
             return # to not execute the code for HOME, DUNGEON, ...
@@ -84,6 +109,39 @@ def handle_input(ch: int):
             if TMP_DATA[SELECTION_OPTIONS[1]] in [0, 1, 2]:
                 GAMESTATE = [1, TMP_DATA[SELECTION_OPTIONS[1]]]
             elif TMP_DATA[SELECTION_OPTIONS[1]] == 3:
+                GAMESTATE = [0]
+        elif len(GAMESTATE) == 4:
+            if TMP_DATA[SELECTION_OPTIONS[1]] == 0: # attack
+                GAMESTATE[3]["health"] -= calculateAttack() # player damages enemy
+                MESSAGES.append(f"You damaged the {GAMESTATE[3]['name']} for {calculateAttack()} damage.")
+
+                if GAMESTATE[3]["health"] > 0: # enemy still alive
+                    PLAYER_DATA["health"] -= min(0, GAMESTATE[3]["attack"]) # enemy damages player
+                    MESSAGES.append(f"{GAMESTATE[3]['name']} attacked you and dealt {GAMESTATE[3]['attack']} damage.")
+                    if PLAYER_DATA["health"] <= 0: # if player dies
+                        GAMESTATE = [0]
+                        MESSAGES.append(f"You died and were ejected from the dungeon.")
+                
+                else: # enemy dead
+                    MESSAGES.append(f"You have defeated the {GAMESTATE[3]['name']}!")
+                    loot = {}
+                    if GAMESTATE[2] > 0: # normal enemy
+                        loot = choice(DUNGEON_MONSTERS_LOOT[GAMESTATE[1]]).copy()
+                    else: # boss
+                        loot = choice(DUNGEON_BOSSES_LOOT[GAMESTATE[1]]).copy()
+
+                    # add loot to inventory                    
+                    if loot["type"] == "coins":
+                        MESSAGES.append(f"You received {loot['amount']} coins!")
+                        PLAYER_DATA["coins"] += loot["amount"]
+                    else:
+                        MESSAGES.append(f"You picked up a {loot['type']}!")
+                        PLAYER_DATA["inventory"].append(loot)
+                    
+                    GAMESTATE[2] -= 1 # advance one enemy
+                    GAMESTATE.pop(3) # remove enemy (indicates that a new one should be added in handle_input)
+            elif TMP_DATA[SELECTION_OPTIONS[1]] == 1: # flee
+                MESSAGES.append("You fled from the dungeon.")
                 GAMESTATE = [0]
 
     if SELECTION_OPTIONS[0] == -2: # revert to no selection
